5a17f0058ae6aaf46f76702db060e9bd316cda73
Wesley Strange <wesley.ws.strange@gmail.com>
2020-07-30 11:16:47 -0700

    Connect to BooksAPI

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
 import { Link } from 'react-router-dom';
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './utils/BooksAPI'
 import './App.css'
 
-class Search extends Component {
+class SearchWindow extends Component {
   render() {
     return (
       <div className="search-books">
@@ -64,24 +64,6 @@ class BookCase extends Component {
                       <div className="book-authors">Harper Lee</div>
                     </div>
                   </li>
-                  <li>
-                    <div className="book">
-                      <div className="book-top">
-                        <div className="book-cover" style={{ width: 128, height: 188, backgroundImage: 'url("http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api")' }}></div>
-                        <div className="book-shelf-changer">
-                          <select>
-                            <option value="move" disabled>Move to...</option>
-                            <option value="currentlyReading">Currently Reading</option>
-                            <option value="wantToRead">Want to Read</option>
-                            <option value="read">Read</option>
-                            <option value="none">None</option>
-                          </select>
-                        </div>
-                      </div>
-                      <div className="book-title">Ender's Game</div>
-                      <div className="book-authors">Orson Scott Card</div>
-                    </div>
-                  </li>
                 </ol>
               </div>
             </div>
@@ -105,24 +87,6 @@ class BookCase extends Component {
                       </div>
                       <div className="book-title">1776</div>
                       <div className="book-authors">David McCullough</div>
-                    </div>
-                  </li>
-                  <li>
-                    <div className="book">
-                      <div className="book-top">
-                        <div className="book-cover" style={{ width: 128, height: 192, backgroundImage: 'url("http://books.google.com/books/content?id=wrOQLV6xB-wC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72G3gA5A-Ka8XjOZGDFLAoUeMQBqZ9y-LCspZ2dzJTugcOcJ4C7FP0tDA8s1h9f480ISXuvYhA_ZpdvRArUL-mZyD4WW7CHyEqHYq9D3kGnrZCNiqxSRhry8TiFDCMWP61ujflB&source=gbs_api")' }}></div>
-                        <div className="book-shelf-changer">
-                          <select>
-                            <option value="move" disabled>Move to...</option>
-                            <option value="currentlyReading">Currently Reading</option>
-                            <option value="wantToRead">Want to Read</option>
-                            <option value="read">Read</option>
-                            <option value="none">None</option>
-                          </select>
-                        </div>
-                      </div>
-                      <div className="book-title">Harry Potter and the Sorcerer's Stone</div>
-                      <div className="book-authors">J.K. Rowling</div>
                     </div>
                   </li>
                 </ol>
@@ -150,42 +114,6 @@ class BookCase extends Component {
                       <div className="book-authors">J.R.R. Tolkien</div>
                     </div>
                   </li>
-                  <li>
-                    <div className="book">
-                      <div className="book-top">
-                        <div className="book-cover" style={{ width: 128, height: 174, backgroundImage: 'url("http://books.google.com/books/content?id=1q_xAwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE712CA0cBYP8VKbEcIVEuFJRdX1k30rjLM29Y-dw_qU1urEZ2cQ42La3Jkw6KmzMmXIoLTr50SWTpw6VOGq1leINsnTdLc_S5a5sn9Hao2t5YT7Ax1RqtQDiPNHIyXP46Rrw3aL8&source=gbs_api")' }}></div>
-                        <div className="book-shelf-changer">
-                          <select>
-                            <option value="move" disabled>Move to...</option>
-                            <option value="currentlyReading">Currently Reading</option>
-                            <option value="wantToRead">Want to Read</option>
-                            <option value="read">Read</option>
-                            <option value="none">None</option>
-                          </select>
-                        </div>
-                      </div>
-                      <div className="book-title">Oh, the Places You'll Go!</div>
-                      <div className="book-authors">Seuss</div>
-                    </div>
-                  </li>
-                  <li>
-                    <div className="book">
-                      <div className="book-top">
-                        <div className="book-cover" style={{ width: 128, height: 192, backgroundImage: 'url("http://books.google.com/books/content?id=32haAAAAMAAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72yckZ5f5bDFVIf7BGPbjA0KYYtlQ__nWB-hI_YZmZ-fScYwFy4O_fWOcPwf-pgv3pPQNJP_sT5J_xOUciD8WaKmevh1rUR-1jk7g1aCD_KeJaOpjVu0cm_11BBIUXdxbFkVMdi&source=gbs_api")' }}></div>
-                        <div className="book-shelf-changer">
-                          <select>
-                            <option value="move" disabled>Move to...</option>
-                            <option value="currentlyReading">Currently Reading</option>
-                            <option value="wantToRead">Want to Read</option>
-                            <option value="read">Read</option>
-                            <option value="none">None</option>
-                          </select>
-                        </div>
-                      </div>
-                      <div className="book-title">The Adventures of Tom Sawyer</div>
-                      <div className="book-authors">Mark Twain</div>
-                    </div>
-                  </li>
                 </ol>
               </div>
             </div>
@@ -204,6 +132,16 @@ class BooksApp extends Component {
     books: []
   }
 
+  componentDidMount() {
+    BooksAPI.getAll()
+      .then((books) => {
+        console.log(books);
+        this.setState(() => ({
+          books
+        }))
+      })
+  }
+
   render() {
     return (
       <div className="app">
@@ -211,7 +149,7 @@ class BooksApp extends Component {
           <BookCase />
         )} />
         <Route path='/search' render={() => (
-          <Search />
+          <SearchWindow />
         )} />
       </div>
     )
